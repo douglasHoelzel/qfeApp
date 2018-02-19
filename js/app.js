6fee8d0d55f4f1bbb563b70cd1331d92738522a7
Doug <douglas.hoelzel@gmail.com>
2018-02-19 14:22:55 -0500

    clean website started, angular and bootstrap only, boilerplate code initialized

--- js/app.js
+++ js/app.js
@@ -3,34 +3,13 @@ app.config(function($routeProvider) {
     .when("/", {
         templateUrl : "home.html"
     })
-    .when("/meetingNotes", {
-        templateUrl : "meetingNotes.html"
-    })
-    .when("/contactUs", {
-        templateUrl : "contactUs.html"
-    })
-    .when("/meetingSchedule", {
-        templateUrl : "meetingSchedule.html"
-    })
-    .when("/teamRules", {
-        templateUrl : "teamRules.html"
-    })
-    .when("/projectConcept", {
-        templateUrl : "projectConcept.html"
-    })
-    .when("/userStories", {
-        templateUrl : "userStories.html"
-    })
-    .when("/specifications", {
-        templateUrl : "specifications.html"
-    })
-    .when("/platformSelection", {
-        templateUrl : "platformSelection.html"
-    })
-    .when("/architectureDiagram", {
-        templateUrl : "architectureDiagram.html"
-    })
     .when("/home", {
         templateUrl : "home.html"
+    })
+    .when("/graphView", {
+        templateUrl : "graphView.html"
+    })
+    .when("/selectedGraph", {
+        templateUrl : "selectedGraph.html"
     });
 });
