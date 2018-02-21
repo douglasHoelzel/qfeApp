772912a2814d8394a89848848e8d526efdf2a4da
Doug <douglas.hoelzel@gmail.com>
2018-02-21 15:29:58 -0500

    added second visualization chart to Graph View page

--- js/MainController.js
+++ js/MainController.js
@@ -14,68 +14,116 @@ app.controller('myCtrl', function($scope, $http, $location) {
     // highcharts
     $scope.getChart = function createChart() {
         console.log("inside of getChart()");
-        var myChart = Highcharts.chart('container', {
+        $scope.myChart = Highcharts.chart('chart1Container', {
             title: {
-            text: 'Solar Employment Growth by Sector, 2010-2016'
-            },
+                text: 'Visualization I'
+                  },
+                  yAxis: {
+                      title: {
+                          text: 'Percent Return'
+                      }
+                  },
+                   xAxis: {
+                      title: {
+                          text: 'Time'
+                      }
+                  },
+                  legend: {
+                      layout: 'vertical',
+                      align: 'right',
+                      verticalAlign: 'middle'
+                  },
 
-            subtitle: {
-            text: 'Source: thesolarfoundation.com'
-            },
+                  plotOptions: {
+                      series: {
+                          label: {
+                              connectorAllowed: false
+                          },
+                          pointStart: 2010
+                      }
+                  },
 
-            yAxis: {
-            title: {
-                text: 'Number of Employees'
-            }
-            },
-            legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-            },
+                  series: [{
+                      name: 'Portfolio',
+                      data: [43934, 52503, 57177, 52503, 97031, 119931, 137133, 154175]
+                  }, {
+                      name: 'Benchmark',
+                      data: [12908, 51503, 52177, 55503, 90031, 109931, 127133, 160175]
+                  }],
 
-            plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 2010
-            }
-            },
-
-            series: [{
-            name: 'Installation',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-            }, {
-            name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-            }, {
-            name: 'Sales & Distribution',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-            }, {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-            }, {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-            }],
-
-            responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-            }
-
+                  responsive: {
+                      rules: [{
+                          condition: {
+                              maxWidth: 500
+                          },
+                          chartOptions: {
+                              legend: {
+                                  layout: 'horizontal',
+                                  align: 'center',
+                                  verticalAlign: 'bottom'
+                              }
+                          }
+                      }]
+                  }
                             });
+          $scope.myChart2 = Highcharts.chart('chart2Container', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Visualization II'
+                },
+                xAxis: {
+                    categories: ['Var1', 'Var2', 'Var3', 'Var4', 'Var5']
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Time'
+                    },
+                    stackLabels: {
+                        enabled: true,
+                        style: {
+                            fontWeight: 'bold',
+                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                        }
+                    }
+                },
+                legend: {
+                    align: 'right',
+                    x: -30,
+                    verticalAlign: 'top',
+                    y: 25,
+                    floating: true,
+                    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                    borderColor: '#CCC',
+                    borderWidth: 1,
+                    shadow: false
+                },
+                tooltip: {
+                    headerFormat: '<b>{point.x}</b><br/>',
+                    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        dataLabels: {
+                            enabled: true,
+                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'Asset1',
+                    data: [5, 3, 4, 7, 2]
+                }, {
+                    name: 'Asset2',
+                    data: [2, 2, 3, 2, 1]
+                }, {
+                    name: 'Asset3',
+                    data: [3, 4, 4, 2, 5]
+                }]
+            });
     };
 
 
@@ -93,7 +141,6 @@ app.controller('myCtrl', function($scope, $http, $location) {
     // Changes view on click of create chart button
     $scope.changeView = function(){
         $location.path("graphView");
-        $scope.getChart();
     }
 
     // Next function
