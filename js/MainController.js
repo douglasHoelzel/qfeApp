5cdd1993d1ba20fe34d2af36207a10e15574905f
Doug <douglas.hoelzel@gmail.com>
2018-03-08 14:15:20 -0500

    got chart working with data

--- js/MainController.js
+++ js/MainController.js
@@ -1,137 +1,71 @@
 var app = angular.module('myApp', ["ngRoute"]);
 app.controller('myCtrl', function($scope, $http, $location) {
-    $scope.investableUniverse;
-    $scope.assets;
-    $scope.objectiveFunction;
-    $scope.rebalancingFrequency;
-    $scope.benchmark;
+    $scope.investableUniverse = "US Equities";
+    $scope.objectiveFunction = "Sharpe Ratio";
+    $scope.frequency = "monthly";
+    $scope.benchmark = "S&P500";
+    $scope.startDate;
+    $scope.endDate;
     $scope.objectiveFunction;
     $scope.location;
-    $scope.loading = false;
     $scope.myChart;
-    $scope.startDate;
-    $scope.toDate;
-    $scope.fromDate;
-    $scope.endDate;
     $scope.usEquityList = ["IYJ","IVV","IYH","IBB", "IYW", "IYF", "IYE", "IYC", "IHE"];
-    $scope.startDate = "2015-12-31";
-    $scope.endDate = "2016-06-30";
-    $scope.frequency = "monthly";
-
-    // Datepicker
-    $('.datepicker').datepicker();
+    //Line Chart Variables
+    $scope.optimizedReturnsDates = [];
+    $scope.optimizedReturnsValues = [];
+    // Bar Chart Variables
+    $scope.optimizedWeightsDates = [];
 
 
     // highcharts
     $scope.getChart = function getChart() {
-        console.log("Inside of get charts function");
+        // Line Chart
         $scope.myChart = Highcharts.chart('chart1Container', {
             title: {
                 text: 'Visualization I'
                   },
-                  yAxis: {
-                      title: {
-                          text: 'Percent Return'
-                      }
+                  yAxis: {title: {text: 'Percent Return'}},
+                  xAxis: {
+                   categories: $scope.optimizedReturnsDates,
+                   tickInterval: 1,
+                   labels: {enabled: true}
                   },
-                   xAxis: {
-                      title: {
-                          text: 'Time'
-                      }
+                  legend: {layout: 'vertical',align: 'right',verticalAlign: 'middle'},
+                  plotOptions: {spline: {marker: {enabled: true}}
                   },
-                  legend: {
-                      layout: 'vertical',
-                      align: 'right',
-                      verticalAlign: 'middle'
-                  },
-
-                  plotOptions: {
-                      series: {
-                          label: {
-                              connectorAllowed: false
-                          },
-                          pointStart: 2010
-                      }
-                  },
-
-                  series: [{
-                      name: 'Portfolio',
-                      data: [43934, 52503, 57177, 52503, 97031, 119931, 137133, 154175],
-                      color: '#3498DB'
-                  }, {
-                      name: 'Benchmark',
-                      data: [12908, 51503, 52177, 55503, 90031, 109931, 127133, 160175],
-                      color: '#76D7C4'
-                  }],
-
-                  responsive: {
-                      rules: [{
-                          condition: {
-                              maxWidth: 500
-                          },
-                          chartOptions: {
-                              legend: {
-                                  layout: 'horizontal',
-                                  align: 'center',
-                                  verticalAlign: 'bottom'
-                              }
-                          }
-                      }]
-                  }
-                            });
+                  series: [{name: 'Portfolio',data:  $scope.optimizedReturnsValues,color: '#3498DB'}],
+                  responsive: {rules: [{condition: {maxWidth: 500},
+                  chartOptions: {legend: {layout: 'horizontal', align: 'center', verticalAlign: 'bottom'}}
+                  }]}
+              });
+          // Bar Chart
           $scope.myChart2 = Highcharts.chart('chart2Container', {
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Visualization II'
-                },
-                xAxis: {
-                    categories: ['Var1', 'Var2', 'Var3', 'Var4', 'Var5']
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: 'Time'
-                    },
-                    stackLabels: {
-                        enabled: true,
-                        style: {
+                chart: {type: 'column'},
+                title: {text: 'Visualization II'},
+                xAxis: {categories: $scope.optimizedWeightsDates,},
+                yAxis: { min: 0, title: { text: 'Time' },
+                    stackLabels: {enabled: true,style: {
                             fontWeight: 'bold',
-                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                        }
+                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'}
                     }
                 },
-                legend: {
-                    align: 'right',
-                    x: -30,
-                    verticalAlign: 'top',
-                    y: 25,
-                    floating: true,
+                legend: {align: 'right',x: -30,verticalAlign: 'top',y: 25,floating: true,
                     backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
                     borderColor: '#CCC',
                     borderWidth: 1,
                     shadow: false
                 },
-                tooltip: {
-                    headerFormat: '<b>{point.x}</b><br/>',
-                    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-                },
-                plotOptions: {
-                    column: {
-                        stacking: 'normal',
-                        dataLabels: {
-                            enabled: true,
-                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-                        }
+                tooltip: {headerFormat: '<b>{point.x}</b><br/>',pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'},
+                plotOptions: {column: {stacking: 'normal',
+                dataLabels: {enabled: true, color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'}
                     }
                 },
                 series: [{
-                    name: 'Asset1',
+                    name: $scope.usEquityList[0],
                     color: '#5DADE2',
                     data: [5, 3, 4, 7, 2]
                 }, {
-                    name: 'Asset2',
+                    name: $scope.usEquityList[1],
                     color: '#76D7C4',
                     data: [2, 2, 3, 2, 1]
                 }, {
@@ -143,30 +77,33 @@ app.controller('myCtrl', function($scope, $http, $location) {
     };
 
     // Ajax Call for Data
-    $scope.getData = function(investableUniverse, assets, objectiveFunction, rebalancingFrequency, benchmark){
-        console.log("IU: " + investableUniverse + " ||  Asset: " + assets + " || OF: " +
-                    objectiveFunction + " || RF: " + rebalancingFrequency + " || BM: " + benchmark);
+    $scope.getData = function(investableUniverse, assets, objectiveFunction, frequency, benchmark, startDate, endDate){
+        $scope.investableUniverse = investableUniverse;
+        $scope.usEquityList = assets;
+        $scope.frequency = frequency.toLowerCase();
+        $scope.startDate = Date.parse(startDate).toString("yyyy-MM-dd");
+        $scope.endDate = Date.parse(endDate).toString("yyyy-MM-dd");
         $('#myOverlay').show();
-        console.log("Getting data....");
+        console.log("Starting AJAX Call....");
             $.ajax({
                   type: "POST",
                   url: "https://api523-nmchenry.cloudapps.unc.edu/api/info",
                   data: JSON.stringify({
-
                               assets: $scope.usEquityList,
-                              start_date: $scope.startDate,
-                              end_date: $scope.endDate,
+                              start_date: "2015-12-31",
+                              end_date: "2016-06-30",
                               frequency: $scope.frequency
                         }),
                   contentType: "application/json; charset=UTF-8'",
                   dataType: "json",
                   success: function(data) {
-                      console.log("Data from Ajax Call below:")
-                      console.log(JSON.stringify(data));
                       $scope.parseData(data);
                   },
                   error: function(data){
-                      console.log("AJAX Call Failed");
+                      swal({
+                        type: 'error',
+                        text: 'Error, Try submitting again',
+                      })
                       $('#myOverlay').hide();
                   },
                   complete: function (data) {
@@ -198,15 +135,19 @@ app.controller('myCtrl', function($scope, $http, $location) {
         })
     }
 
-    // Parses data coming from api
+    // Parses All Data Coming from API
     $scope.parseData = function(data){
+        console.log(data);
         console.log("Inside of data parser");
-
-        var plainData = data;
-        console.log("JSON plain: " + plainData);
-
-
-
-
+        // Parses All Optimized Returns Data and Saves it
+        for (var key in data.optimized_returns) {
+                    $scope.optimizedReturnsDates.push(key);
+                    $scope.optimizedReturnsValues.push(data.optimized_returns[key]);
+        }
+        // Parses All Optimized Weights Dates
+        for (var key in data.optimized_weights) {
+                    $scope.optimizedWeightsDates.push(key);
+        }
+        // Parses All
     }
 });
