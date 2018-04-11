69a93eb17675eb738189eb476c1494a11ec48a41
Doug <douglas.hoelzel@gmail.com>
2018-04-11 16:04:42 -0400

    fixed visualizations

--- js/MainController.js
+++ js/MainController.js
@@ -5,10 +5,12 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
     $scope.objectiveFunction = "Sharpe Ratio";
     $scope.frequency = "monthly";
     $scope.benchmark = "S&P500";
+    $scope.benchmarkList = ["IVV"];
     $scope.startDate;
     $scope.endDate;
     $scope.objectiveFunction;
     $scope.location;
+    $scope.transaction_costs;
     $scope.myChart;
     $scope.usEquityList = ["IYJ","IVV","IYH","IBB", "IYW", "IYF", "IYE", "IYC", "IHE"];
     $rootScope.optimizedReturnsDates = [];
@@ -18,6 +20,19 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
     $rootScope.benchmarkReturnsDates = [];
     $rootScope.benchmarkReturnsValues = [];
     $scope.benchmarkWeightsDates = [];
+    $scope.benchmarkDataNames = [];
+
+    $scope.optimizedWeightsAsset1 = [];
+    $scope.optimizedWeightsAsset2 = [];
+    $scope.optimizedWeightsAsset3 = [];
+    $scope.optimizedWeightsAsset4 = [];
+    $scope.optimizedWeightsAsset5 = [];
+    $scope.optimizedWeightsAsset6 = [];
+    $scope.optimizedWeightsAsset7 = [];
+    $scope.optimizedWeightsAsset8 = [];
+    $scope.optimizedWeightsAsset9 = [];
+
+
 
     $rootScope.myChart;
     $rootScope.myChart2;
@@ -28,7 +43,7 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
         // Line Chart
         $rootScope.myChart = Highcharts.chart('chart1', {
             title: {
-                text: 'Visualization I'
+                text: 'Cumulative Returns'
                   },
                   yAxis: {title: {text: 'Percent Return'}},
                   xAxis: {
@@ -39,7 +54,9 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
                   legend: {layout: 'vertical',align: 'right',verticalAlign: 'middle'},
                   plotOptions: {spline: {marker: {enabled: true}}
                   },
-                  series: [{name: 'Portfolio',data:  $rootScope.optimizedReturnsValues,color: '#3498DB'}],
+                  series: [ { name: 'Portfolio', data:  $rootScope.optimizedReturnsValues, color: '#3498DB' } ,
+                            { name: 'Benchmark', data:  $rootScope.benchmarkReturnsValues, color: '#DB3434' }
+                          ],
                   exporting: { buttons: {customButton: {text: 'Enlarge',
                    onclick: function () {
                        $scope.selectChartClick("1");
@@ -59,11 +76,11 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
                 chart: {type: 'column'},
                 title: {text: 'Visualization II'},
                 xAxis: {categories: $scope.optimizedWeightsDates,},
-                yAxis: { min: 0, title: { text: 'Time' },
-                    stackLabels: {enabled: true,style: {
-                            fontWeight: 'bold',
-                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'}
-                    }
+                yAxis: { min: 0, title: { text: 'Time' } //,
+                    //stackLabels: {enabled: true,style: {
+                       //     fontWeight: 'bold',
+                       //     color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'}
+                   // }
                 },
                 legend: {align: 'right',x: -30,verticalAlign: 'top',y: 25,floating: true,
                     backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
@@ -72,19 +89,20 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
                     shadow: false
                 },
                 tooltip: {headerFormat: '<b>{point.x}</b><br/>',pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'},
-                plotOptions: {column: {stacking: 'normal',
-                dataLabels: {enabled: true, color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'}
+                plotOptions: {column: {stacking: 'normal' //,
+                //dataLabels: {enabled: true, color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'}
                     }
                 },
                 series: [
-                    { name: $scope.usEquityList[0], color: '#5DADE2', data: [5, 3, 4, 7, 2, 5, 3, 4, 7] },
-                    { name: $scope.usEquityList[1], color: '#76D7C4', data: [2, 2, 3, 2, 1, 2, 3, 2, 1] },
-                    { name: $scope.usEquityList[2], color: '#BFC9CA', data: [3, 4, 4, 2, 5, 4, 4, 2, 5] },
-                    { name: $scope.usEquityList[3], color: '#5ED483', data: [3, 4, 4, 2, 5, 4, 4, 2, 5] },
-                    { name: $scope.usEquityList[4], color: '#4AADA7', data: [3, 4, 4, 2, 5, 4, 4, 2, 5] },
-                    { name: $scope.usEquityList[5], color: '#4C6CAB', data: [3, 4, 4, 2, 5, 4, 4, 2, 5] },
-                    { name: $scope.usEquityList[6], color: '#4968E9', data: [3, 4, 4, 2, 5, 4, 4, 2, 5] },
-                    { name: $scope.usEquityList[7], color: '#47E0B2', data: [3, 4, 4, 2, 5, 4, 4, 2, 5] },
+                    { name: $scope.usEquityList[0], color: '#5DADE2', data: $scope.optimizedWeightsAsset1 },
+                    { name: $scope.usEquityList[1], color: '#76D7C4', data: $scope.optimizedWeightsAsset2 },
+                    { name: $scope.usEquityList[2], color: '#BFC9CA', data: $scope.optimizedWeightsAsset3 },
+                    { name: $scope.usEquityList[3], color: '#5ED483', data: $scope.optimizedWeightsAsset4 },
+                    { name: $scope.usEquityList[4], color: '#4AADA7', data: $scope.optimizedWeightsAsset5 },
+                    { name: $scope.usEquityList[5], color: '#4C6CAB', data: $scope.optimizedWeightsAsset6 },
+                    { name: $scope.usEquityList[6], color: '#4968E9', data: $scope.optimizedWeightsAsset7 },
+                    { name: $scope.usEquityList[7], color: '#47E0B2', data: $scope.optimizedWeightsAsset8 },
+                    { name: $scope.usEquityList[8], color: '#47E0B2', data: $scope.optimizedWeightsAsset9 },
                     ],
                 exporting: { buttons: {customButton: {text: 'Enlarge',
                  onclick: function () {
@@ -98,23 +116,23 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
     };
 
     // Ajax Call for Data
-    $scope.getData = function(investableUniverse, assets, objectiveFunction, frequency, benchmark, startDate, endDate){
+    $scope.getData = function(investableUniverse, assets, objectiveFunction, frequency, benchmark, startDate, endDate, transaction_costs){
         $scope.investableUniverse = investableUniverse;
         $scope.usEquityList = assets;
+        $scope.transaction_costs = transaction_costs;
         $scope.frequency = frequency.toLowerCase();
         $scope.startDate = Date.parse(startDate).toString("yyyy-MM-dd");
         $scope.endDate = Date.parse(endDate).toString("yyyy-MM-dd");
         console.log("SD: "+ $scope.startDate + " ED: " + $scope.endDate);
         $('#myOverlay').show();
         $rootScope.loading = true;
-
         console.log("Starting AJAX Call....");
             $.ajax({
                   type: "POST",
-                  // type: "GET",
+                   //type: "GET",
                   url: "https://api523-nmchenry.cloudapps.unc.edu/api/info",
-                  // url: "https://api523-nmchenry.cloudapps.unc.edu/api/test",
-                  data: JSON.stringify({ assets: $scope.usEquityList, benchmark: "IVV", start_date: $scope.startDate, end_date: $scope.endDate, frequency: $scope.frequency, transaction_costs: "0" }),
+                   //url: "https://api523-nmchenry.cloudapps.unc.edu/api/test",
+                  data: JSON.stringify({ assets: $scope.usEquityList, benchmark: $scope.benchmarkList, start_date: $scope.startDate, end_date: $scope.endDate, frequency: $scope.frequency, transaction_costs: $scope.transaction_costs }),
                   contentType: "application/json; charset=UTF-8'",
                   dataType: "json",
                   success: function(data) {
@@ -165,24 +183,35 @@ app.controller('myCtrl', function($scope, $http, $location, $rootScope) {
     // Parses All Data Coming from API
     $scope.parseData = function(data){
         console.log("Inside of data parser");
-        // Parses All Optimized Returns Data and Saves it
-        for (var key in data.optimized_returns) {
-                    $rootScope.optimizedReturnsDates.push(key);
-                    $rootScope.optimizedReturnsValues.push(data.optimized_returns[key]);
-        }
+        console.log(data);
+
         // Parses All Optimized Weights Dates
         for (var key in data.optimized_weights) {
                     $scope.optimizedWeightsDates.push(key);
         }
 
-        // Parses All Benchmark Data
-        for (var key in data.benchmark_cumulative_returns) {
-                    $rootScope.benchmarkReturnsDates.push(key);
-                    $rootScope.benchmarkReturnsValues.push(data.benchmark_cumulative_returns[key]);
+        // Parses Benchmark and Portfolio Data
+        for (var key in data.benchmark_portfolio_intersection) {
+                    $rootScope.optimizedReturnsDates.push(key);
+                    $rootScope.optimizedReturnsValues.push(data.benchmark_portfolio_intersection[key][0]);
+                    $rootScope.benchmarkReturnsValues.push(data.benchmark_portfolio_intersection[key][1]);
         }
+        console.log("Dates: " +  $rootScope.optimizedReturnsDates);
+        console.log($rootScope.optimizedReturnsValues);
+        console.log($rootScope.benchmarkReturnsValues);
+
+
         // Parses All Benchmark Data
         for (var key in data.optimized_weights) {
-                    $scope.benchmarkWeightsDates.push(key);
+            $scope.optimizedWeightsAsset1.push(Math.abs(data.optimized_weights[key]['0'][1]));
+            $scope.optimizedWeightsAsset2.push(Math.abs(data.optimized_weights[key]['1'][1]));
+            $scope.optimizedWeightsAsset3.push(Math.abs(data.optimized_weights[key]['2'][1]));
+            $scope.optimizedWeightsAsset4.push(Math.abs(data.optimized_weights[key]['3'][1]));
+            $scope.optimizedWeightsAsset5.push(Math.abs(data.optimized_weights[key]['4'][1]));
+            $scope.optimizedWeightsAsset6.push(Math.abs(data.optimized_weights[key]['5'][1]));
+            $scope.optimizedWeightsAsset7.push(Math.abs(data.optimized_weights[key]['6'][1]));
+            $scope.optimizedWeightsAsset8.push(Math.abs(data.optimized_weights[key]['7'][1]));
+            $scope.optimizedWeightsAsset9.push(Math.abs(data.optimized_weights[key]['7'][1]));
         }
     }
     // Selects Chart and changes view
